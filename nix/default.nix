b4d33fc40ae7367cf3472895e022fc7305c542cb
Tim Cuthbertson <tim@gfxmonk.net>
2021-08-19 11:43:08 +1000

    simplify nix src

--- nix/default.nix
+++ nix/default.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import <nixpkgs> {}, enableNeovim ? false, sourceOverrides ? {} }:
+{ pkgs ? import <nixpkgs> {}, enableNeovim ? false }:
 with pkgs;
 let
 	pythonPackages = pkgs.python3Packages;
@@ -6,11 +6,7 @@ let
 in
 stdenv.mkDerivation {
 	name = "vim-watch";
-	src = sourceOverrides.vim-watch or (fetchgit {
-		url = "https://github.com/gfxmonk/vim-watch";
-		rev = "382aea1469dc832154e7c1aaf7c43923c1973155";
-		sha256 = "1hxv6a14wsjbn0nncgy3nl7kw428zy3l11j415y8nmvkr73y55jn";
-	});
+	src = ../.;
 	buildInputs = [ python ] ++ (if enableNeovim then [ makeWrapper neovim-remote ] else []);
 	installPhase = ''
 		mkdir -p $out/share/vim;
